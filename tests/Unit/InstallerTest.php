7226922cc9cb0f5f95943f9de1fe0984b0aca58f
Dirk Wenzel <wenzel@cps-it.de>
2018-12-18 16:20:08 +0100

    [TASK] Installer stub implemented (WIP)

--- tests/Unit/InstallerTest.php
+++ tests/Unit/InstallerTest.php
@@ -4,5 +4,34 @@ namespace Cpsit\Conductor\Tests\Unit;
 
 class InstallerTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * {@inheritDoc}
+     *
+     * @throws \PHPUnit\Framework\Exception
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->installer = new Installer();
+        $this->io = $this->createMock(IOInterface::class);
+        $this->composer = $this->createMock(Composer::class);
+        $this->eventDispatcher = $this->getMockBuilder(EventDispatcher::class)->disableOriginalConstructor()->getMock();
+        $this->composer->expects(self::any())->method('getEventDispatcher')->willReturn($this->eventDispatcher);
+    }
+
+    public function testGetSubscribedEvents(): void
+    {
+        $events = Installer::getSubscribedEvents();
+        self::assertSame(
+            [
+                'post-install-cmd' => 'dumpVersionsClass',
+                'post-update-cmd' => 'dumpVersionsClass',
+            ],
+            $events
+        );
+        foreach ($events as $callback) {
+            self::assertInternalType('callable', [$this->installer, $callback]);
+        }
+    }
 
 }
